bccfc530801e0a4a7457181a32d07f6314ccb6a1
Zizou <57289047+Zzzizou@users.noreply.github.com>
2021-01-11 14:41:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -168,3 +168,11 @@ robot.on('raw', event => {
     };
   };
 });
+
+
+robot.user.setPresence({
+        game: {
+          name: `Visual Studio Code\nEditing: discord.rb`, /* если интересует, как сделать "прошло" - отпишите в комментарии */
+          type: 'LISTENING"
+        }
+      })
